04c678ee96377eb5fed3ec874ab130cd033d0362
Lynette Lim <lynette.lim@kuleuven.vib.be>
2025-11-19 16:20:39 -0800

    Delete Figures/Main directory

--- Figures/Main/Figure 7/D_E/LRP-Dach1/getxy_cent.m
+++
@@ -1,8 +0,0 @@
-function xy_cent = getxy_cent (ch_t)
-stats1 = regionprops(ch_t, 'Centroid');
-centroids1 = cat(1, stats1.Centroid);
-cr1 = round (centroids1); 
-x1 = cr1 (:, 1);
-y1 = cr1 (:, 2); 
-xy_cent = [x1, y1];
-end
